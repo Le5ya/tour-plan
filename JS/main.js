105cc848bd3c8a2f57bc0109e16e94e1059ac257
Leya Leya <liliatolstyh@yandex.ru>
2020-09-23 21:24:00 +0300

    create: review slider

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,27 @@
-var mySwiper = new Swiper(".swiper-container", {
+var hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: ".slider-button--next",
-    prevEl: ".slider-button--prev",
+    nextEl: ".hotel-slider__button--next",
+    prevEl: ".hotel-slider__button--prev",
   },
   effect: "coverflow",
+  keyboard: {
+    enabled: true,
+    onlyViewport: false,
+  },
+});
+var reviewsSlider = new Swiper(".reviews-slider", {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".hotel-slider__button--prev",
+    prevEl: ".shotel-slider__button--next",
+  },
   keyboard: {
     enabled: true,
     onlyViewport: false,
