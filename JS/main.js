d413a4129703d0d34c009bb015bcb69ae429f94f
Leya Leya <liliatolstyh@yandex.ru>
2020-10-03 13:06:26 +0300

    close modal by Esc

--- JS/main.js
+++ JS/main.js
@@ -50,4 +50,13 @@ $(document).ready(function () {
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
   }
+
+  $(document).keyup(function (e) {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    if (e.key === "Escape" || e.keyCode === 27) {
+      modalOverlay.removeClass("modal__overlay--visible");
+      modalDialog.removeClass("modal__dialog--visible");
+    }
+  });
 });
