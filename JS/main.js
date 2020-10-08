17dff9941809583bab971878b9bf19fac5674cea
Leya Leya <liliatolstyh@yandex.ru>
2020-10-08 22:35:18 +0300

    form validate

--- JS/main.js
+++ JS/main.js
@@ -59,4 +59,18 @@ $(document).ready(function () {
       modalDialog.removeClass("modal__dialog--visible");
     }
   });
-});
+
+  //Обработка форм
+  $(".modal__form").validate({
+    errorClass: "invalid",
+    messages: {
+      name: {
+        requiered: "Укажите имя",
+        minlength: "Имя не короче двух букв",
+      },
+      email: {
+        reqired: "We need your email adress to contact you",
+        email: "Your email address must be in format of name@domain.com",
+      },
+    },
+  });
